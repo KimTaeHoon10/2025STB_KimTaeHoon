6e877cb08dddd9f8379b084564343af25bb315a3
KimTaeHoon10 <cissalc1999@gmail.com>
2025-09-26 14:06:32 +0900

    Update 04.Data_Graph(3).R

--- 04.Data_Graph(3).R
+++ 04.Data_Graph(3).R
@@ -1,17 +1,33 @@
-table(X2025 $Gender)#진출국가
+#step2
+table(X2025 $Gender)
 
+#step3
+ECN <- table(X2025 $Gender)
+prop.table(ECN)
 
-ECN <- table(X2025 $Gender)#ECN에 진출국가의 도수분포표를 대입
-prop.table(ECN)#상대 도수로 표시
-
+#step4
 table(X2025 $Gender, X2025 $Grade)
 
-
+#step5
 barplot(table(X2025 $`Nationality`))
+
+#step6
 barplot(table(X2025 $`residential area`), ylab = "거주지역", xlim=c(0,60), horiz=TRUE)
+
+#step7
 entry <- table(X2025 $`Grade`, X2025 $`Gender`)
 barplot(entry, legend = TRUE)
+
+#step8
 pie(table(X2025 $Grade))
+
+#step9
 hist(X2025$Age, main="나이분포", col=terrain.colors(15))
-boxplot(X2025$Grade, X2025$Age, main="나이와 학년 비교", col="yellow", names = c("학년","나이"))
-plot(x=X2025$Grade, y=X2025$Age, xlab="학년", ylab="나이", main="학년별 나이 비교", pch=24, col="red", bg="yellow", cex=1.5)
+
+#step10
+boxplot(Age ~ Grade, data = X2025, main = "학년별 나이 분포", xlab = "학년", ylab = "나이", col = "yellow")
+aggregate(Age ~ Grade, data = X2025, summary)
+
+#step11
+plot(x=X2025$Grade, y=X2025$Age, xlab="학년", ylab="나이", main="학년별 나이 비교", pch=24, col="red", bg="yellow", cex=1.5,  xaxt = "n") 
+axis(side = 1, at = c(2, 3, 4), labels = c("2", "3", "4"))
